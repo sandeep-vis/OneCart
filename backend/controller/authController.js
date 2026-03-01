7ad1f88e827dc88fc4f0386ed04f7573002ec035
sandeep vishwakarma <sandeep30122001@gmil.com>
2026-03-01 23:54:00 +0530

    Fixed logout cookie clear options for production

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -72,15 +72,29 @@ export const login = async (req, res) => {
     }
 }
 
+// export const logOut = async (req, res) => {
+//     try {
+//         res.clearCookie("token")
+//         return res.status(200).json({ message: "logOut successful" })
+//     } catch (error) {
+//         console.log("logOut error")
+//         return res.status(500).json({ message: `LogOut error ${error}` })
+//     }
+
+// }
+
 export const logOut = async (req, res) => {
     try {
-        res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        })
         return res.status(200).json({ message: "logOut successful" })
     } catch (error) {
         console.log("logOut error")
         return res.status(500).json({ message: `LogOut error ${error}` })
     }
-
 }
 
 
